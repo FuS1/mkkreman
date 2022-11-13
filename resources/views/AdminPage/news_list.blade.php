c6884192847de945ef8622172d534dc76a3cf171
injwang <a271954@gmail.com>
2022-11-14 01:38:25 +0800

    調整SideMenu自適應

--- resources/views/AdminPage/news_list.blade.php
+++ resources/views/AdminPage/news_list.blade.php
@@ -80,10 +80,12 @@
 	</script>
 </head>
 
-<body class="flex">
-	@relativeInclude('layout.side_menu')
-	<div class="bg-gray-50 py-5 w-full">
-		<div class="px-5">
+<body class="flex bg-gray-50">
+	<div class="md:w-1/3 lg:w-1/4 xl:w-1/6 2xl:w-1/7">
+		@relativeInclude('layout.side_menu')
+	</div>
+	<div class="md:w-2/3 lg:w-3/4 xl:w-5/6 2xl:w-6/7">
+		<div class="py-5 px-5 w-full">
 			<div class="mb-2">
 				<h1 class="text-1xl md:text-2xl font-bold text-gray-600 inline-block align-middle">最新消息設定</h1>
 			</div>
