f7f5be1d682c33b400f5e3afae1e7117e3163e52
injwang <a271954@gmail.com>
2022-11-15 01:16:06 +0800

    完成後台帳戶功能

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use DateTimeInterface;
 use Kirschbaum\PowerJoins\PowerJoins;
+use DateTimeInterface;
 
-class Admin extends Authenticatable 
+class Admin extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     protected $table = 'admin';
@@ -18,4 +18,15 @@ class Admin extends Authenticatable
     protected $hidden = ['account','password'];
     protected $guarded = [];
 
+
+    /**
+     * 为数组 / JSON 序列化准备日期。
+     *
+     * @param  \DateTimeInterface  $date
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format($this->dateFormat ?: 'Y-m-d H:i:s');
+    }
 }
