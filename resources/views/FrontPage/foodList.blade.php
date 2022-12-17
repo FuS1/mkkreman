fbcda3f65cf1cc0fdadc6b7165d65d39eac9f1ad
injwang <a271954@gmail.com>
2022-12-18 04:31:57 +0800

    調整食物滑動效果

--- resources/views/FrontPage/foodList.blade.php
+++ resources/views/FrontPage/foodList.blade.php
@@ -4,12 +4,28 @@
     @relativeInclude('include.meta')
 <style>
    
-    .food-swiper .swiper-slide > img {
+    .food-swiper .swiper-slide .content-bottom > img {
         width:100%;
     }
 
-    .food-swiper .swiper-slide-next > img{
-        transform: scale(1.8);
+    @media screen and (min-width: 768px) {
+    
+        .food-swiper .swiper-slide .content-bottom > img {
+            transform: scale(0.75);
+            margin-top: 3rem;
+        }
+        .food-swiper .swiper-slide-next .content-bottom > img{
+            transform: scale(1);
+            margin-top: 0rem;
+        }
+    }
+
+    .food_info{
+        margin-top: 3rem;
+        text-align:center;
+    }
+    .food_info > .description{
+        text-align:center;
     }
 
 </style>
@@ -33,16 +49,18 @@
                 <div class="swiper-wrapper">
                 @foreach ($mainFood as $_mainFood)
                     <div class="swiper-slide">
-                        <img src="{{ asset( 'storage/'.$_mainFood->file_path ) }}" alt="">
+                        <div class="content-bottom">
+                            <img src="{{ asset( 'storage/'.$_mainFood->file_path ) }}" alt="">
+                        </div>
                     </div>
                 @endforeach
                 </div>
                 <div class="swiper-button-next"></div>
                 <div class="swiper-button-prev"></div>
             </div>
-            <div>
-                <h2>招牌豚骨拉麵</h2>
-                <div>
+            <div class="food_info">
+                <h2><strong>招牌豚骨拉麵</strong></h2>
+                <div class="description">
                     豬大骨湯底，透著淡淡的牡蠣白，營養不油膩；<br>
                     搭配舞火智商的叉燒肉<br>
                     香氣撲鼻，美味無雙，初訪必點。
@@ -62,7 +80,7 @@
         var swiperNews = new Swiper(".food-swiper", {
             slidesPerView: 1.5,
             spaceBetween: 30,
-            // loop: true,
+            loop: true,
             speed: 800,     
             slidesPerGroup: 1,   
             centeredSlides:true,
@@ -70,20 +88,17 @@
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
             },
-            // pagination: {
-            //     el: ".swiper-pagination",
-            //     clickable: true,
-            // },
             breakpoints: {
+                // 768: {
+                //     slidesPerView: 2,
+                //     slidesPerGroup: 2,
+                //     centeredSlides:false,
+                // },
                 768: {
-                    slidesPerView: 2,
-                    slidesPerGroup: 2,
-                    centeredSlides:false,
-                },
-                1200: {
+                    loop: false,
                     slidesPerView: 3,
                     slidesPerGroup: 3,  
-                    spaceBetween: 120,
+                    spaceBetween: 60,
                     centeredSlides:false,
                 }
             }
