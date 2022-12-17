dde862f56c4d31af58e5eac07dbba5b01748a140
injwang <a271954@gmail.com>
2022-12-18 05:01:22 +0800

    調整標題樣式

--- resources/views/FrontPage/foodList.blade.php
+++ resources/views/FrontPage/foodList.blade.php
@@ -24,8 +24,15 @@
         margin-top: 3rem;
         text-align:center;
     }
+    .mainFood > h2{
+        font-family: "KaiminSoStd";
+        text-align: center;
+        margin: 0 0 2.5rem 0;
+        color: #132C53;
+    }
     .food_info > .description{
         text-align:center;
+        line-height: 2rem;
     }
 
 </style>
@@ -45,27 +52,32 @@
             <div class="title">
                 <img src="{{ asset('FrontPage/public/img/title-food.svg') }}" alt="阿良真誠推薦">
             </div>
-            <div class="swiper-container food-swiper">
-                <div class="swiper-wrapper">
-                @foreach ($mainFood as $_mainFood)
-                    <div class="swiper-slide">
-                        <div class="content-bottom">
-                            <img src="{{ asset( 'storage/'.$_mainFood->file_path ) }}" alt="">
+
+            <div class="mainFood">
+                <h2>〈 拉麵系列 〉</h2>
+                <div class="swiper-container food-swiper">
+                    <div class="swiper-wrapper">
+                    @foreach ($mainFood as $_mainFood)
+                        <div class="swiper-slide">
+                            <div class="content-bottom">
+                                <img src="{{ asset( 'storage/'.$_mainFood->file_path ) }}" alt="">
+                            </div>
                         </div>
+                    @endforeach
                     </div>
-                @endforeach
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
                 </div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
+                <div class="food_info">
+                    <h2><strong>招牌豚骨拉麵</strong></h2>
+                    <div class="description">
+                        豬大骨湯底，透著淡淡的牡蠣白，營養不油膩；<br>
+                        搭配舞火智商的叉燒肉<br>
+                        香氣撲鼻，美味無雙，初訪必點。
+                    </div>
+                </div>  
             </div>
-            <div class="food_info">
-                <h2><strong>招牌豚骨拉麵</strong></h2>
-                <div class="description">
-                    豬大骨湯底，透著淡淡的牡蠣白，營養不油膩；<br>
-                    搭配舞火智商的叉燒肉<br>
-                    香氣撲鼻，美味無雙，初訪必點。
-                </div>
-            </div>  
+            
 
         </div>
 
@@ -95,9 +107,9 @@
                 //     centeredSlides:false,
                 // },
                 768: {
-                    loop: false,
+                    loop: true,
                     slidesPerView: 3,
-                    slidesPerGroup: 3,  
+                    slidesPerGroup: 1,  
                     spaceBetween: 60,
                     centeredSlides:false,
                 }
