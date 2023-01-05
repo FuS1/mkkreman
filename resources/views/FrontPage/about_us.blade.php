8a4dfdd29a8b502be59cc067ceec93fdf1f06993
injWang <a271954@gmail.com>
2023-01-05 10:37:59 +0800

    整合樣式

--- resources/views/FrontPage/about_us.blade.php
+++ resources/views/FrontPage/about_us.blade.php
@@ -32,17 +32,80 @@
                                     <img class="person" src="{{ asset('FrontPage/public/img/character-1.svg') }}" alt="">
                                     <!-- <img class="say" src="./public/img/say.svg" alt=""> -->
                                 </div>
+                                <div class="about_character_text">
+                                    <div class="inner">
+                                        <div style="border: 0.15rem dashed #122C53;text-align: center;padding: 4px 3px;width: fit-content;font-size: 17px;font-weight: bold;margin: auto;margin-bottom: 15px;letter-spacing: 9px;">夢想是開一間拉麵店</div>
+                                        <ul>
+                                            <li>暱稱：爆子頭 阿良</li>
+                                            <li>出身：台灣鄉下</li>
+                                            <li>生日：9/6 處女座(目前26歲)</li>
+                                            <li>興趣：美食和交朋友</li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                             <div class="swiper-slide">
                                 <div class="about_character_pic">
                                     <img class="person" src="{{ asset('FrontPage/public/img/character-2.svg') }}" alt="">
                                     <!-- <img class="say" src="./public/img/say.svg" alt=""> -->
                                 </div>
+                                <div class="about_character_text">
+                                    <div class="inner">
+                                        <div style="border: 0.15rem dashed #122C53;text-align: center;padding: 4px 3px;width: fit-content;font-size: 17px;font-weight: bold;margin: auto;margin-bottom: 15px;letter-spacing: 9px;">夢想是開一間拉麵店</div>
+                                        <ul>
+                                            <li>暱稱：爆子頭 阿良</li>
+                                            <li>出身：台灣城市</li>
+                                            <li>生日：10/6 天秤座(目前26歲)</li>
+                                            <li>興趣：美食和交朋友</li>
+                                        </ul>
+                                    </div>
+                                </div>
                             </div>
                             <div class="swiper-slide">
                                 <div class="about_character_pic">
                                     <img class="person" src="{{ asset('FrontPage/public/img/character-3.svg') }}" alt="">
                                     <!-- <img class="say" src="./public/img/say.svg" alt=""> -->
+                                </div>
+                                <div class="about_character_text">
+                                    <div class="inner">
+                                        <div style="border: 0.15rem dashed #122C53;text-align: center;padding: 4px 3px;width: fit-content;font-size: 17px;font-weight: bold;margin: auto;margin-bottom: 15px;letter-spacing: 9px;">夢想是開一間拉麵店</div>
+                                        <ul>
+                                            <li>暱稱：爆子頭 阿良</li>
+                                            <li>出身：台灣漁村</li>
+                                            <li>生日：11/6 天蠍座(目前26歲)</li>
+                                            <li>興趣：美食和交朋友</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>    
+                    </div>
+                    <div class="swiper-button-next character_arrow" id="character_arrow_next"></div>
+                    <div class="swiper-button-prev character_arrow" id="character_arrow_prev"></div> 
+                </div>
+            </div>
+
+            <!-- <div class="about_character">
+                <div class="container">
+                    <div class="title">
+                        <img src="{{ asset('FrontPage/public/img/title-role.svg') }}" alt="麵匡匡人物">
+                        <h2>Character Introduction</h2>
+                    </div>
+                    <div class="swiper-container characterPic">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <div class="about_character_pic">
+                                    <img class="person" src="{{ asset('FrontPage/public/img/character-1.svg') }}" alt="">
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="about_character_pic">
+                                    <img class="person" src="{{ asset('FrontPage/public/img/character-2.svg') }}" alt="">
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="about_character_pic">
+                                    <img class="person" src="{{ asset('FrontPage/public/img/character-3.svg') }}" alt="">
                                 </div>
                             </div>
                         </div>     
@@ -93,7 +156,7 @@
                         <div class="swiper-button-prev"></div>
                     </div>
                 </div>
-            </div>
+            </div> -->
             
             <div class="about_btn">
                 <div class="container">
@@ -118,11 +181,11 @@
             spaceBetween: 20,
             centeredSlides: true,
             loop: true,
-            allowTouchMove: false,
+            // allowTouchMove: false,
             speed: 800,
             navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
+                nextEl: "#character_arrow_next",
+                prevEl: "#character_arrow_prev",
             },
             breakpoints: {
                 600: {
@@ -135,21 +198,26 @@
                 },
             },
         });
-        var swiperText = new Swiper(".characterText", {
-            slidesPerView: 1,
-            loop: true,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-            thumbs: {
-                swiper: swiperPic,
-            },
-            effect: "fade",
-            speed: 800,
-            fadeEffect: {
-                crossFade: true,
-            },
+
+        function characterIntro() {
+            let introHeight_max, introHeight_prev = 0;
+            $(".about_character_text").each(function(){
+                let introHeight = $(this).height();
+                if(introHeight > introHeight_max) {
+                    introHeight_max = introHeight;
+                }else {
+                    introHeight_prev = introHeight;
+                }
+            })
+            introHeight_max = introHeight_max + 24;
+            $(".characterPic").css("padding-bottom","calc(55px + " + introHeight_max + "px)");
+            $(".character_arrow").each(function(){
+                $(this).css("bottom","calc(" + introHeight_max + "px / 2)");
+            })
+        }
+        characterIntro();
+        $(window).on("resize scroll",function(){
+            characterIntro();
         });
     </script>
     
