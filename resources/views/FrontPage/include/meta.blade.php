66b437aa43e02b4f07a6cc4a42603972a063ee3e
injwang <a271954@gmail.com>
2023-01-09 22:41:34 +0800

    調整 meta

--- resources/views/FrontPage/include/meta.blade.php
+++ resources/views/FrontPage/include/meta.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>麵匡匡</title>
+<title>麵匡匡拉麵食堂</title>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.6.16"></script>
@@ -10,4 +10,10 @@
 <link rel="stylesheet" href="{{ asset('FrontPage/lib/css/bootstrap/bootstrap.css') }}">
 <link rel="stylesheet" href="{{ asset('FrontPage/lib/css/swiper/swiper-bundle.min.css') }}">
 <link rel="stylesheet" href="{{ asset('FrontPage/public/css/style.css') }}"> 
-
+<meta property="og:type" content="type" />  
+<meta property="og:title" content="{{ config('env.APP_CHINESE_NAME') }}" />
+<meta property="og:description" content="{{ config('env.APP_CHINESE_DESCRIPTION') }}" />
+<meta property="og:image" content="{{ mix('/FrontPage/public/img/favicon.png') }}" />
+<meta property="og:url" content="{{ config('env.APP_URL') }}" />
+<meta property="og:site_name" content="{{ config('env.APP_CHINESE_NAME') }}" />
+<link rel="icon" href="{{ mix('/FrontPage/public/img/favicon.png') }}">
